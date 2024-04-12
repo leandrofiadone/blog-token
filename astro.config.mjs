2ea3398ec139b0e451f7fd2d80ad15dcbcc76b82
Leandro Fiadone <leanfiadone@gmail.com>
2024-04-12 15:33:39 -0300

    finalizada primera parte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-import {defineConfig} from "astro/config"
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com"
-})
+  site: "https://example.com",
+  integrations: [preact({
+    compat: true
+  })]
+});
